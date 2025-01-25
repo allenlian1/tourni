354c19cc71b4f7d9c438fbdc5b3bf10a8ac0b487
Ram989 <ramiljiwani@gmail.com>
2025-01-25 12:39:46 -0500

    account page

--- src/app/account/page.tsx
+++ src/app/account/page.tsx
@@ -3,40 +3,41 @@ import NavBar from "@/components/navbar";
 import { Button } from "@/components/ui/button";
 import { Table, TableBody, TableCaption, TableCell, TableFooter, TableHead, TableHeader, TableRow } from "@/components/ui/table";
 import { logout } from "@/lib/actions/auth";
+import {
+  Avatar,
+  AvatarFallback,
+  AvatarImage,
+} from "@/components/ui/avatar"
 
 export default async function AccountPage() {
   const session = await auth();
     return (
       <div>
-        <h1 className="scroll-m-20 text-4xl font-semibold tracking-tight lg:text-3xl">
-          Account
+        <h1 className="flex justify-center mt-10">
+          <Avatar className="h-16 w-16">
+            <AvatarImage src={session?.user?.image!} alt="@shadcn" />
+            <AvatarFallback>CN</AvatarFallback>
+          </Avatar>
         </h1>
-        <Table>
-          <TableCaption></TableCaption>
-          <TableHeader>
-            <TableRow>
-              <TableHead className="w-[100px]">Invoice</TableHead>
-              <TableHead>Status</TableHead>
-              <TableHead>Method</TableHead>
-              <TableHead className="text-right">Amount</TableHead>
-            </TableRow>
-          </TableHeader>
-          <TableBody>
-              <TableRow>
-                <TableCell className="font-medium">Data 1</TableCell>
-                <TableCell>Data 2</TableCell>
-                <TableCell>Data 3</TableCell>
-                <TableCell className="text-right">Data 4</TableCell>
-              </TableRow>
-          </TableBody>
-          <TableFooter>
-            <TableRow>
-              <TableCell colSpan={3}>Total</TableCell>
-              <TableCell className="text-right">$2,500.00</TableCell>
-            </TableRow>
-          </TableFooter>
-        </Table>
-        <Button variant="destructive" onClick={logout}>Sign Out</Button>
+        <div className=" flex justify-center scroll-m-20 text-4xl font-semibold tracking-tight lg:text-3xl mt-4">
+          {session?.user?.name}
+        </div>
+        <div className=" flex justify-center scroll-m-20 text-medium font-semibold tracking-tight text-gray-500 lg:text-3xl mt-1">
+          {session?.user?.email}
+        </div>
+        <div className="flex justify-center mt-4">
+          <img
+            src="playerCard.png"
+            alt="Player Card"
+            className="rounded-lg shadow-lg"
+          />
+        </div>
+
+        <div className="flex justify-center mt-4">
+          <Button className="bg-white text-muted hover:bg-muted hover:text-white" variant="destructive" onClick={logout}>
+            Sign Out
+          </Button>
+        </div>
       </div>
     );
   }
