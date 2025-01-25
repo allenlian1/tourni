91eaf97763dc58a7b13008f1f967371a9eed73f1
Allen <jyallen06@gmail.com>
2025-01-25 18:11:42 -0500

    Tournaments search UI & fetches

--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -5,11 +5,14 @@ import { Slider } from "@/components/ui/slider";
 export default function SearchPage() {
     return (
       <div className="w-full flex-col justify-center items-center">
-        <div className="w-11/12 flex flex-col justify-center items-center">
-          <h1 className="scroll-m-20 text-4xl font-semibold tracking-tight lg:text-3xl">
+        <div className="w-11/12 flex flex-col justify-center items-center pt-4 pl-4">
+          <h1 className="w-full text-align-left text-2xl sm:text-3xl font-bold mb-4 sm:mb-6">
             Search
           </h1>
           <Slider />
+          <div>
+            
+          </div>
         </div>
       </div>
     );
