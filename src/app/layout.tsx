b84c204562c54ca5ca45400083b58a58ab10faf7
Allen <jyallen06@gmail.com>
2025-01-26 04:46:54 -0500

    oliad

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import NavBar from "@/components/navbar";
 import { auth } from "@/auth";
+import { Providers } from './providers';
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -28,7 +29,7 @@ export default async function RootLayout({
   return (
     <html lang="en">
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
-        {children} {/* This renders the page content */}
+      <Providers>{children}</Providers> {/* This renders the page content */}
         {session?.user && <NavBar/>}
       </body>
     </html>
